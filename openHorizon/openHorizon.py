8dbb53c0883daad161954c3ab48a6433c7f7f4db
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2024-02-27 10:30:26 +0100

    Update openHorizon.py

--- openHorizon/openHorizon.py
+++ openHorizon/openHorizon.py
@@ -18,8 +18,7 @@ class MyOpenHorizon:
         self._password = userAuth.split(":")[1]
         self._headers = {'Content-Type': 'application/json'}
         self.status = self.get_status()
-        self._exchangeAPI = self.status["exchange_api"]
-        # self._exchangeAPI = None if self.status is None else self.status["exchange_api"]
+        self._exchangeAPI = None if self.status is None else self.status["exchange_api"]
         self.nodes = MyOpenHorizonNode(url=url, org=org, userName=self._userName, password=self._password, headers=self._headers, exchangeAPI=self._exchangeAPI)
         self.policies = MyOpenHorizonPolicies(url=url, org=org, userName=self._userName, password=self._password, headers=self._headers, exchangeAPI=self._exchangeAPI)
         self.services = MyOpenHorizonService(url=url, org=org, userName=self._userName, password=self._password, headers=self._headers, exchangeAPI=self._exchangeAPI)
@@ -32,14 +31,12 @@ class MyOpenHorizon:
         """
 
         # Check if the service is running
-       # try:
-       #     status = requests.get("http://160.85.252.236:8510/status", headers=self._headers)
-       # except requests.exceptions.RequestException as e:
-       #     print(e)
-       #     return None
-        status = "exchange_api"
-        return status["exchange_api"]
-        
+        try:
+           status = requests.get("http://160.85.252.236:8510/status", headers=self._headers)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return None      
+            
         status = status.json()
         
         # Check if the status contains the configuration
